3479733680c7e464b2f0cdd5a1214d57415280fe
Ridenour <jeffrey.ridenour@igt.com>
2021-06-30 16:15:45 -0700

    adding audio deploy script as part of template

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# lfs-template
-template for creating  LFS repos for psd, layout project  and audio repos
+# slot-template-audio-howler
+Repository for Audio Team to place audio assets
+
+Usage: please alter the settings.json to conform to the needs of your project.
+
+To copy the audio files from this repo into your game repo, run the copyAudio.js script in this folder.  The script reads in configuration params from the adjacent settings.json file so make sure and update those with the correct relative paths to your game project for your game.
+
+To run the script, type:
+
+$node copyAudio.js
+
+
+
---
+++ copyAudio.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const exec = require("child_process").exec;
+
+const settings = JSON.parse(fs.readFileSync("settings.json"));
+
+const audio = new Map(Object.entries(settings || {}));
+
+const gameProjectPath = audio.get('gameProjectPath');
+
+
+function copyDirectory(path) {
+    fs.readdirSync(path).forEach(element => {
+            const filePath = path + "/" + element;
+            let destPath = null;
+            if (!element.startsWith(".") && fs.existsSync(filePath) && !fs.lstatSync(filePath).isDirectory()) {
+                //process and copy file
+                console.log("process " + element);
+                destPath = gameProjectPath + "/assets/default/default/default/sounds/soundFiles/";
+
+                if (destPath) {
+                    console.log("copy from " + filePath + " to " + destPath + element);
+                    if (!fs.existsSync(destPath)) {
+                        fs.mkdirSync(destPath,  { recursive: true });
+                    }
+                    fs.copyFileSync(filePath, destPath + element);
+                }
+            }
+        }
+    )
+}
+
+function copySoundConfigToGame(path){
+    if(!fs.existsSync(path + "/sounds.json")){
+        console.log( path+ "/sounds.json is missing, skipping");
+
+    } else {
+        let filePath = path + "/sounds.json";
+        let destPath = gameProjectPath + "/assets/default/default/default/sounds";
+        console.log("copy from " + filePath + " to " + destPath + "/sounds.json");
+        if (!fs.existsSync(destPath)) {
+            fs.mkdirSync(destPath,  { recursive: true });
+        }
+        fs.copyFileSync(filePath, destPath + "/sounds.json");
+    }
+    if (!fs.existsSync(path + "/sounds.json5")) {
+        console.log(path + "/sounds.json5 is missing, skipping");
+
+    } else {
+        let filePath = path + "/sounds.json5";
+        let destPath = gameProjectPath + "/assets/default/default/default/sounds";
+        console.log("copy from " + filePath + " to " + destPath + "/sounds.json5");
+        if (!fs.existsSync(destPath)) {
+            fs.mkdirSync(destPath, { recursive: true });
+        }
+        fs.copyFileSync(filePath, destPath + "/sounds.json5");
+    }
+
+}
+
+function copySoundsToGame(path) {
+    if (!fs.existsSync(path)) {
+        console.log("Sounds Folder" + " missing, skipping...");
+    }
+    else {
+        copyDirectory(path);
+    }
+}
+
+if(audio !== "") {
+    console.log("audio files:");
+    console.log(gameProjectPath);
+
+    if(!fs.existsSync(gameProjectPath + "/assets")) {
+        console.log("Game Path " + gameProjectPath + "/assets" + " missing, skipping...");
+    } else {
+        copySoundConfigToGame(".");
+        copySoundsToGame("./soundFiles");
+    }
+}
